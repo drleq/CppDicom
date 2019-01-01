a54918c414dd8acb9dbfb4f70e873f037146b5a5
Andrew Condie <andrew.condie@gmail.com>
2019-01-01 18:01:15 +0000

    Add launch targets for MSVC. Add launch target for gdb DicomFileTest. Deleted some old regression test code that no longer applies.

--- DicomFileTest/main.cpp
+++ DicomFileTest/main.cpp
@@ -23,99 +23,6 @@ using namespace dicom;
 using namespace dicom::io;
 using namespace dicom::io::file;
 
-//#define DATASET_TESTS 0
-
-#ifdef DATASET_TESTS
-int main(int, const char*[]) {
-    using namespace std::filesystem;
-    const path DatasetsShare{ "\\\\edidicom\\edidicom\\Company_Data\\Datasets" };
-
-    DataDictionaryPtr data_dictionary(new DataDictionary);
-    tags::populate_data_dictionary(data_dictionary);
-    FileMetadataSource dicom_source;
-
-
-    std::ofstream log_file{ "AccessLog.txt", std::ios_base::out | std::ios_base::app };
-
-    const int StartDatasetNumber = 6001;
-    const int MaxDatasetNumber = 9200;
-    int dataset_number = StartDatasetNumber;
-    while (true) {
-        if (dataset_number > MaxDatasetNumber) {
-            break;
-        }
-
-        int range_low = ((dataset_number / 100) * 100) + 1;
-        int range_high = range_low + 99;
-
-        std::stringstream range_dir_str;
-        range_dir_str << std::setfill('0') << std::setw(5) << range_low << '-' << std::setw(5) << range_high;
-        path range_dir = DatasetsShare / range_dir_str.str();
-
-        dataset_number += 100;
-        if (!exists(range_dir)) {
-            continue;
-        }
-
-        directory_iterator range_dir_it{ range_dir };
-        directory_iterator range_dir_end;
-        for (; range_dir_it != range_dir_end; ++range_dir_it) {
-            if (!is_directory(range_dir_it->status())) {
-                continue;
-            }
-
-            path patient_dir = range_dir_it->path();
-            auto patient_dir_length = patient_dir.string().size() + 1; // +1 for separator
-            log_file << patient_dir.c_str() << std::endl;
-            std::wcout << patient_dir.c_str() << std::endl;
-
-            try {
-                recursive_directory_iterator file_it{ patient_dir };
-                recursive_directory_iterator file_end_it;
-
-                size_t file_count = 0;
-                auto start = std::chrono::high_resolution_clock::now();
-
-                for (; file_it != file_end_it; ++file_it) {
-                    if (!is_regular_file(file_it->status()) && !is_symlink(file_it->status())) {
-                        continue;
-                    }
-
-                    path dicom_file = file_it->path();
-                    std::string short_path = dicom_file.string().substr(patient_dir_length);
-
-                    try {
-                        auto load_result = dicom_source.Load(dicom_file.c_str(), data_dictionary);
-                        if (load_result == nullptr) {
-                            log_file << "    Load failed: " << short_path << std::endl;
-                        }
-
-                        file_count++;
-                    } catch (const std::exception& e) {
-                        log_file << "    Exception: " << short_path << std::endl;
-                        log_file << "        " << e.what() << std::endl;
-                    } catch (...) {
-                        log_file << "    Exception: " << short_path << std::endl;
-                    }
-                }
-
-                auto end = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double, std::milli> delta = end - start;
-                log_file <<
-                    "    " << file_count << " files in " << delta.count() << "ms" <<
-                    "  (" << (delta.count() / file_count) << "ms each)" <<
-                    std::endl;
-
-            } catch (const std::exception& e) {
-                log_file << "    Exception: " << e.what() << std::endl;
-            } catch (...) {
-                log_file << "    Exception!" << std::endl;
-            }
-        }
-    }
-}
-
-#else // #ifdef DATASET_TESTS
 const int LoopCount = 1000;
 
 int main(int argc, const char* argv[]) {
@@ -152,4 +59,3 @@ int main(int argc, const char* argv[]) {
 
     return 0;
 }
-#endif
